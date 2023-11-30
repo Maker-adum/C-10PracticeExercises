ffc61b533875ead7eb2e51af4594e7dd5110f87e
Adum Awan <adummaker02@gmail.com>
2023-11-30 21:33:39 +0300

    Initial commit

---
+++ CSharp10FundamentalsExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
+
+Console.WriteLine("Which city were you born?");
+string cityOfBirth = Console.ReadLine();
+
+Console.WriteLine("Which Country were you born");
+string countryOfBirth = Console.ReadLine();
+
+Console.WriteLine("Hello " + name + ", you were born in " + countryOfBirth + " in the city of " + cityOfBirth);
