b2da25a22648f1693669c9d71793f8d01d76a706
Adum Awan <adummaker02@gmail.com>
2023-12-03 20:28:49 +0300

    Initial commit

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name, favoriteMovie;
+
+Console.WriteLine("Enter your name");
+name = Console.ReadLine();
+
+Console.WriteLine("Enter your favorite movie");
+favoriteMovie = Console.ReadLine();
+
+Console.WriteLine("Hello " + name + " " + "your favorite movie is " + favoriteMovie);
