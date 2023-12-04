c62f837e2158b1646a971bbe28c66292eeddf016
Adum Awan <adummaker02@gmail.com>
2023-12-04 17:22:14 +0300

    fifth commit

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// numbers
+Console.WriteLine("Enter the first number: ");
+double x = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter the second number: ");
+double y = double.Parse(Console.ReadLine());
+
+// addition
+double sum = x + y;
+
+// subtraction
+double difference = x - y;
+
+// multiplication
+double product = x * y;
+
+// division
+double quotient = x / y;
+
+// results
+Console.WriteLine($"Addition: {x} + {y} = {sum}");
+Console.WriteLine($"Subtraction: {x} - {y} = {difference}");
+Console.WriteLine($"Multiplication: {x} * {y} = {product}");
+Console.WriteLine($"Division: {x} / {y} = {quotient}");
