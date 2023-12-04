22d822a2cafc0300127484b1a413a8a62687c4aa
Adum Awan <adummaker02@gmail.com>
2023-12-04 12:16:20 +0300

    Forth commit

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Implicit typing
+var implicitVariable = 50; // Compiler infers the type based on the assigned value (int in this case)
+Console.WriteLine("Implicit Variable " + implicitVariable);
+
+// Explicit typing
+int explicitVariable = 20; // Type is explicitly specified as int
+Console.WriteLine("Explicit Variable " + explicitVariable);
