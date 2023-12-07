65b29e1b02be93275423486faf200fed434cb5fa
Adum Awan <adummaker02@gmail.com>
2023-12-08 00:05:12 +0300

    seventh commit

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Please enter your age: ");
+string input = Console.ReadLine();
+int age = int.Parse(input);
+
+switch (age)
+{
+    case int myAge when (myAge >= 18 && myAge <= 24):
+        Console.WriteLine("You are almost eligible to work.");
+        break;
+
+    case int myAge when (myAge >= 25 && myAge <= 70):
+        Console.WriteLine("You are eligible to work.");
+        break;
+
+    default:
+        Console.WriteLine("You are not eligible to work.");
+        break;
+}
+
