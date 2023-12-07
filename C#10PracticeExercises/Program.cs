2a85af912142fcfa7f310bfa532c6e0822867e8d
Adum Awan <adummaker02@gmail.com>
2023-12-08 00:17:15 +0300

    ch4-3x1

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter a letter: "); 
+char letter = Console.ReadLine().ToLower()[0]; // .ToLower conversts a letter to lowercase and
+                                               // [0] used to get the first character of the input string since Console.ReadLine returns a string
+
+if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+{
+    Console.WriteLine("Vowel");
+}
+else
+{
+    Console.WriteLine("Consonant");
+}
