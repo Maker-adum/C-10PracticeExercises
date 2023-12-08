cdb75ed95579577a4fcb42a62008a666f0aaab72
Adum Awan <adummaker02@gmail.com>
2023-12-08 11:45:14 +0300

    ch4-ex5

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+        Random random = new Random();
+
+        // Generate a random number between 1 and 100
+        int targetNumber = random.Next(1, 101);
+
+        int userGuess;
+
+        do
+        {
+            Console.Write("Enter your guess (between 1 and 100): ");
+
+            // Read the user input as a string
+            string userInput = Console.ReadLine();
+
+            // Parse the string to an integer
+            if (int.TryParse(userInput, out userGuess))
+            {
+                if (userGuess < 1 || userGuess > 100)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 100.");
+                }
+                else if (userGuess < targetNumber)
+                {
+                    Console.WriteLine("Too low. Try again.");
+                }
+                else if (userGuess > targetNumber)
+                {
+                    Console.WriteLine("Too high. Try again.");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations! You guessed the number {targetNumber}!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+        } while (userGuess != targetNumber);
+
+
