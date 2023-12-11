25edd4ceaa0bc433b4d0cf54f6be7177d618d7df
Adum Awan <adummaker02@gmail.com>
2023-12-11 11:42:52 +0300

    ch4-ex6

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int number;
+
+for (; ; )
+{
+    Console.Write("Enter a number between 1 and 7: ");
+    number = int.Parse(Console.ReadLine());
+
+    if (number >= 1 && number <= 7)
+        break;
+
+    Console.WriteLine("Invalid input. Please try again.");
+}
+
+string dayOfWeek = "";
+
+switch (number)
+{
+    case 1:
+        dayOfWeek = "Monday";
+        break;
+    case 2:
+        dayOfWeek = "Tuesday";
+        break;
+    case 3:
+        dayOfWeek = "Wednesday";
+        break;
+    case 4:
+        dayOfWeek = "Thursday";
+        break;
+    case 5:
+        dayOfWeek = "Friday";
+        break;
+    case 6:
+        dayOfWeek = "Saturday";
+        break;
+    case 7:
+        dayOfWeek = "Sunday";
+        break;
+    default:
+        dayOfWeek = "Invalid input";
+        break;
+}
+
+Console.WriteLine($"{number} = {dayOfWeek}");
