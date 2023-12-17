cf6c28b0c98731748785156f29694c48ab75ed18
Adum Awan <adummaker02@gmail.com>
2023-12-17 11:39:03 +0300

    Implement Average Age Calculator

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int numberOfAges = 3; 
+
+            int[] ages = new int[numberOfAges];
+            int sum = 0;
+
+            for (int i = 0; i < numberOfAges; i++)
+            {
+                Console.Write($"Enter age {i + 1}: ");
+                ages[i] = int.Parse(Console.ReadLine());
+                sum += ages[i];
+            }
+
+            double averageAge = (double)sum / numberOfAges;
+
+            Console.WriteLine($"The average age is: {averageAge:F2}");  //F2 is a format specifier for floating point number. Specifies that the number should be formated to two decimal places
+        }
+    }
+
