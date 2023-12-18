167720387e87afc73fb6ae2fde14cc8503c2ed49
Adum Awan <adummaker02@gmail.com>
2023-12-18 10:22:34 +0300

    Implemented Utility class for calculator operations and output handling

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,79 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose an operation: ");
+            Console.WriteLine("1. Add");
+            Console.WriteLine("2. Subtract");
+            Console.WriteLine("3. Multiply");
+            Console.WriteLine("4. Divide");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the first value: ");
+            double value1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the second value: ");
+            double value2 = double.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine($"The sum of {value1} and {value2} is: {Utility.Add(value1, value2)}");
+                    break;
+                case 2:
+                    Console.WriteLine($"The difference of {value1} and {value2} is: {Utility.Subtract(value1, value2)}");
+                    break;
+                case 3:
+                    Console.WriteLine($"The product of {value1} and {value2} is: {Utility.Multiply(value1, value2)}");
+                    break;
+                case 4:
+                    double result = Utility.Divide(value1, value2);
+                    if (result != double.MinValue)
+                    {
+                        Console.WriteLine($"The division of {value1} by {value2} is: {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+
+    public static class Utility
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                return double.MinValue;
+            }
+        }
+    }
+}
