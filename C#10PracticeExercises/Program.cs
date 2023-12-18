7b017588bf6891c4b2d560372f1e00919ec83c74
Adum Awan <adummaker02@gmail.com>
2023-12-18 10:12:22 +0300

    Implemented fitness feedback based on exercise hours, with bonus for over 10 hours.

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SportsApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of hours of exercise per week: ");
+            int hoursOfExercise = int.Parse(Console.ReadLine());
+
+            string feedback = GetFitnessLevel(hoursOfExercise);
+
+            Console.WriteLine(feedback);
+        }
+
+        static string GetFitnessLevel(int hours)
+        {
+            string feedback = "";
+
+            if (hours < 0)
+            {
+                feedback = "Invalid input: Number of hours cannot be negative";
+            }
+            else if (hours >= 0 && hours < 3)
+            {
+                feedback = "You should consider increasing the amount of exercise for better fitness.";
+            }
+            else if (hours >= 3 && hours < 7)
+            {
+                feedback = "You are maintaining a moderate level of fitness.";
+            }
+            else if (hours >= 7 && hours <= 10)
+            {
+                feedback = "You are in good shape. Keep it up!";
+            }
+            else
+            {
+                feedback = "Wow! You are very dedicated to fitness. Keep up the great work!";
+                feedback += " You have earned an extra bonus for exercising more than 10 hours per week!";
+            }
+
+            return feedback;
+        }
+    }
+}
