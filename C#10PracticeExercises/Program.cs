0f620884661e26634b99d3e1abb3e494b55e2ba6
Adum Awan <adummaker02@gmail.com>
2023-12-18 11:22:27 +0300

    Random number generator program

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace RandomNumberGeneratorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many random numbers do you want to generate? (default is 10)");
+
+            int numCount;
+            bool isValidInput = int.TryParse(Console.ReadLine(), out numCount);
+            if (!isValidInput || numCount <= 0)
+            {
+                numCount = 10;
+            }
+
+            int[] randomNumbers = GenerateRandomNumbers(numCount);
+
+            Console.WriteLine($"Generated {numCount} random numbers:");
+            foreach (int number in randomNumbers)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+
+        static int[] GenerateRandomNumbers(int count = 10)
+        {
+            Random rand = new Random();
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = rand.Next(1, 101);
+            }
+
+            return numbers;
+        }
+    }
+}
+
