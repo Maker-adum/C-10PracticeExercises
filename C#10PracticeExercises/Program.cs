fe6fad2914c53e505007a15d25459a03e5677003
Adum Awan <adummaker02@gmail.com>
2024-01-14 22:14:52 +0300

    Added a workout tracker console app with personalized congrats message

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Enter the activity: ");
+string activity  = Console.ReadLine();
+
+Console.WriteLine("Enter the number of hours of workout this week: ");
+string hoursOfWorkout = Console.ReadLine();
+
+Console.WriteLine($"{name}, you've crushed it this week! You hav practiced {activity} for {hoursOfWorkout} hours! Keep up the amazing work!");
+Console.WriteLine($"Here is a trophy for your dedication");
