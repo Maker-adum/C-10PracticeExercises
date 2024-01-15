232c9c3a3f2fd68cf3fa87f19a1b395e250b7760
Adum Awan <adummaker02@gmail.com>
2024-01-15 11:59:22 +0300

    Added a helper file that takes the total number of hours as a string argument and convert it to an int value

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using C_10PracticeExercises;
+
+Console.Write("Please enter your name: ");
+string name = Console.ReadLine();
+
+Console.Write("Please enter your activity: ");
+string activity = Console.ReadLine();
+
+Console.Write("Please enter your average hours of workout per day: ");
+string averageHoursPerDay = Console.ReadLine();
+
+int totalHoursPerWeek = Utility.CalculateTotalHoursPerWeek(averageHoursPerDay);
+
+Console.WriteLine();
+Console.WriteLine($"{name}, you're rockin' it! You've practiced {activity} for a total of {totalHoursPerWeek} hours this week! Keep up the amazing work!");
+
+Console.WriteLine($"Here's a shiny medal for your dedication:");
---
+++ C#10PracticeExercises/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_10PracticeExercises
+{
+    public class Utility
+    {
+        public static int CalculateTotalHoursPerWeek(string averageHoursPerDay)
+        {
+            int averageHours = int.Parse(averageHoursPerDay);
+            int totalHoursPerWeek = averageHours * 7;
+            return totalHoursPerWeek;
+        }
+    }
+
+}
+
