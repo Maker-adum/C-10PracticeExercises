5ca13577dd9133cd0545bc92fab1b14fee9808f0
Adum Awan <adummaker02@gmail.com>
2024-01-15 12:31:42 +0300

    Add console app for name recognition

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Read user input
+Console.Write("Enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("Enter your last name: ");
+string lastName = Console.ReadLine();
+
+// Normalize the names by converting them to lowercase
+string normalizedFirstName = firstName.ToLower();
+string normalizedLastName = lastName.ToLower();
+
+// Define the recognized name
+string recognizedFirstName = "mike";
+string recognizedLastName = "london";
+
+// Check if the user's name matches the recognized name
+ if (normalizedFirstName.Contains(recognizedFirstName) && normalizedLastName.Contains(recognizedLastName))
+ {
+    Console.WriteLine("Hello, Mike London! Welcome!");
+ }
+   else
+   
+    Console.WriteLine("Hello! Welcome!");
+ 
+     
