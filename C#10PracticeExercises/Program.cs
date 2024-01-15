0902dac001aacc8e5faa476547d6d05909ab96b8
Adum Awan <adummaker02@gmail.com>
2024-01-15 13:37:54 +0300

    Created an output that prints all data for each property on a new line

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Read user input
+Console.Write("Enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("Enter your last name: ");
+string lastName = Console.ReadLine();
+
+Console.Write("Enter your city: ");
+string city = Console.ReadLine();
+
+Console.Write("Enter your street: ");
+string street = Console.ReadLine();
+
+Console.Write("Enter your P.O. Box: ");
+string poBox = Console.ReadLine();
+
+// Print user information
+Console.WriteLine("First Name: " + firstName);
+Console.WriteLine("Last Name: " + lastName);
+Console.WriteLine("City: " + city);
+Console.WriteLine("Street: " + street);
+Console.WriteLine("P.O. Box: " + poBox);
