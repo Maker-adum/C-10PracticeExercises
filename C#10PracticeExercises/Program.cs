357bbdf287c4ba4af52aeee16572921d222194f1
Adum Awan <adummaker02@gmail.com>
2024-01-16 22:04:19 +0300

    Add functionality to insert random newlines and tabs in a string

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a string: ");
+        string input = Console.ReadLine();
+
+        string output = InsertRandomNewlinesAndTabs(input);
+
+        Console.WriteLine("Modified string:");
+        Console.WriteLine(output);
+    }
+
+    public static string InsertRandomNewlinesAndTabs(string input)
+    {
+        Random random = new();
+        var outputBuilder = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            outputBuilder.Append(c);
+
+            // Randomly insert '\n' and '\t' characters
+            if (random.Next(2) == 0)
+            {
+                outputBuilder.Append(random.Next(2) == 0 ? '\n' : '\t');
+            }
+        }
+
+        return outputBuilder.ToString();
+    }
+}
