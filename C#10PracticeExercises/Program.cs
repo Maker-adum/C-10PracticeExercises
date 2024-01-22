e454698d7db5225e3ee38f112b88463da05c856b
Adum Awan <adummaker02@gmail.com>
2024-01-22 14:34:28 +0300

    Created circle class and a constructor that takes in the value of the radius and calculation method

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Circle
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public double CalculateArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+
+    public double CalculateCircumference()
+    {
+        return 2 * Math.PI * Radius;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the radius of the circle: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+
+        Circle circle = new Circle(radius);
+
+        double area = circle.CalculateArea();
+        double circumference = circle.CalculateCircumference();
+
+        Console.WriteLine("Circle area: " + area);
+        Console.WriteLine("Circle circumference:" + circumference);
+
+    }
+}
