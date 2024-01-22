e98574723843a8566c0d8f3ec9a47828ec196988
Adum Awan <adummaker02@gmail.com>
2024-01-22 15:21:23 +0300

    Implement Book class with properties, constructor, and availability check method

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Book
+{
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? ISBN { get; set; }
+    public bool IsAvailable { get; set; }
+
+
+    public Book(string title, string author, string isbn)
+    {
+        Title = title;
+        Author = author;
+        ISBN = isbn;
+        IsAvailable = true;
+    }
+
+    public void CheckAvailability()
+    {
+        if (IsAvailable)
+        {
+            Console.WriteLine("The book is available to be borrowed. ");
+        }
+
+        else
+        {
+            Console.WriteLine("The book is currently not available for borrowing. ");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string title = "The Great Gatsby";
+        string author = "F. Scott Fitzgerald";
+        string isbn = "9780743273565";
+
+        Book book = new Book(title, author, isbn);
+
+        book.CheckAvailability();
+
+        // Simulating borrowing the book
+        book.IsAvailable = false;
+
+        book.CheckAvailability();
+    }
+}
