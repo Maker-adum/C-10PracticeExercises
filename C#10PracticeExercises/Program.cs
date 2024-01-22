05218f87ee5257e0f60357d5220f73c41edd5d3c
Adum Awan <adummaker02@gmail.com>
2024-01-22 11:08:11 +0300

    Implement Car constructor with modelName, color, and maxSpeed parameter

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Car
+{
+    public string ModelName;
+    public string Color;
+    public int MaxSpeed;
+
+    public Car(string modelName, string color, int maxSpeed)
+    {
+        ModelName = modelName;
+        Color = color;
+        MaxSpeed = maxSpeed;
+    }
+
+    public void StartEngine()
+    {
+        Console.WriteLine("Engine started!");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the car model name: ");
+        string modelName = Console.ReadLine();
+
+        Console.Write("Enter the car color: ");
+        string color = Console.ReadLine();
+
+        Console.Write("Enter the car max speed: ");
+        string speedInput = Console.ReadLine();
+        int.TryParse(speedInput, out int maxSpeed);
+
+        Car myCar = new Car(modelName, color, maxSpeed);
+
+        Console.WriteLine("Car model name: " + myCar.ModelName);
+        Console.WriteLine("Car color: " + myCar.Color);
+        Console.WriteLine("Car max speed: " + myCar.MaxSpeed);
+
+        myCar.StartEngine();
+    }
+}
