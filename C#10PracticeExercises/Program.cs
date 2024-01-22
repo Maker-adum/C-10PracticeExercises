8a626bcc02c7496125d61e744898d2c0c2a83b86
Adum Awan <adummaker02@gmail.com>
2024-01-22 10:48:36 +0300

    Implement Car class with parameters and object creation in Main using dot syntax

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Car
+{
+    public string Color;
+    public int MaxSpeed;
+
+    public void StartEngine()
+    {
+        Console.WriteLine("Engine started!");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Car myCar = new Car();
+
+        Console.Write("Enter the car color: ");
+        myCar.Color = Console.ReadLine();
+
+        Console.Write("Enter the car max speed: ");
+        string speedInput = Console.ReadLine();
+        int.TryParse(speedInput, out myCar.MaxSpeed);
+
+        Console.WriteLine("Car color: " + myCar.Color);
+        Console.WriteLine("Car max speed: " + myCar.MaxSpeed);
+
+        myCar.StartEngine();
+    }
+}
