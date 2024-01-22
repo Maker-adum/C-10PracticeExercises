021dc3dd68186539f99a410d38344222377cebea
Adum Awan <adummaker02@gmail.com>
2024-01-22 11:12:01 +0300

    Implement Rectangle class with properties, constructor, area, and perimeter calculation

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Rectangle
+{
+    public double Length { get; set; }
+    public double Width { get; set; }
+
+    public Rectangle(double length, double width)
+    {
+        Length = length;
+        Width = width;
+    }
+
+    public double CalculateArea()
+    {
+        return Length * Width;
+    }
+
+    public double CalculatePerimeter()
+    {
+        return 2 * (Length + Width);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the length of the rectangle: ");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the width of the rectangle: ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(length, width);
+
+        double area = rectangle.CalculateArea();
+        double perimeter = rectangle.CalculatePerimeter();
+
+        Console.WriteLine("Rectangle area: " + area);
+        Console.WriteLine("Rectangle perimeter: " + perimeter);
+    }
+}
