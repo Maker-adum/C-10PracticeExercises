076f23a38b921bf391702eecca73cce4d286b4ac
Adum Awan <adummaker02@gmail.com>
2024-01-27 22:21:43 +0300

    Add reusable Swap method to swap two integer variables

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        int x = 5;
+        int y = 10;
+
+        Console.WriteLine("Before swap:");
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("y = " + y);
+
+        Swap(ref x, ref y);
+
+        Console.WriteLine("\nAfter swap:");
+        Console.WriteLine("x = " + x);
+        Console.WriteLine("y = " + y);
+
+    }
+
+    public static void Swap(ref int a, ref int b)
+    {
+
+        int temp = a;
+
+        a = b;
+        b = temp;
+
+    }
+
+}
