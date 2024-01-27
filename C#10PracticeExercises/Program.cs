4aeafdbf9511525723427026f26556e509a70954
Adum Awan <adummaker02@gmail.com>
2024-01-27 22:02:27 +0300

    Implement string copying using String.CopyTo()

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string string1 = "Hello Word";
+
+        string string2 = String.Copy(string1);
+
+        Console.WriteLine("Original String (string1): " + string1);
+        Console.WriteLine("Copied String (string2): " + string2);
+    }
+}
