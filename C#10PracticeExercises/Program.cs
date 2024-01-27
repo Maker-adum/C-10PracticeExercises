7eba062ea20e3e09f5cfb30e12cd596115cc4dfc
Adum Awan <adummaker02@gmail.com>
2024-01-27 23:20:04 +0300

    "Add enum 'Months' and method to get month name by number"

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public enum Months
+{
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
+
+public class Program
+{
+    public static string GetMonthName(int monthNumber)
+    {
+        if (Enum.IsDefined(typeof(Months), monthNumber))
+        {
+            Months month = (Months)monthNumber;
+            return month.ToString();
+        }
+        else
+        {
+            return "Invalid month number";
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a month number (1-12): ");
+        int monthNumber = Convert.ToInt32(Console.ReadLine());
+
+        string monthName = GetMonthName(monthNumber);
+        Console.WriteLine("Month: " + monthName);
+    }
+}
