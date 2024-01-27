2985c7f4b1ba382262dd4ad692b311972c4bc6f4
Adum Awan <adummaker02@gmail.com>
2024-01-27 22:26:38 +0300

    Concatenate two strings using String.Concat() method

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string str1 = "Hello, ";
+        string str2 = "World!";
+
+        string result = String.Concat(str1, str2);
+
+        Console.WriteLine(result);
+    }
+}
