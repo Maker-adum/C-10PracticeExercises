b2dc0907b2b7f50807db0ccae5b3b36ddfeaab6c
Adum Awan <adummaker02@gmail.com>
2024-01-27 21:46:03 +0300

    Add method to multiply sum of two ints by the first int

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Calculation
+{
+    public static int MultiplySumByFirst(int firstNumber, int secondNumber)
+    {
+        int sum = firstNumber + secondNumber;
+        int result = sum * firstNumber;
+        return result;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the first number");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter second number");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        int result = Calculation.MultiplySumByFirst(firstNumber, secondNumber);
+        Console.WriteLine("result: " + result);  // Output: 40
+    }
+}
