c858a79d477f4473624b06099a6fc8ec03ceaeab
Adum Awan <adummaker02@gmail.com>
2024-01-27 22:35:10 +0300

    Convert string case using ToUpper() and ToLower() methods

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string str1 = "Hello, World";
+        string str2 = "Learn CSharp";
+
+        string str3 = str1.ToUpper();
+        string str4 = str2.ToLower();
+
+        Console.WriteLine("Original String: " + str1);
+        Console.WriteLine("Uppercase String: " + str3);
+        Console.WriteLine("Lowercase String: " + str4);
+    }
+}
