026fc7ca2d1347b08ca20d101062201271ef15a7
Adum Awan <adummaker02@gmail.com>
2024-02-06 23:20:04 +0300

    Added a simple C# console application demonstrating basic usage of custom math functions for addition and subtraction.

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using MyMath;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result1 = Addition.Add(5, 3);
+            Console.WriteLine("Result of Addition: " + result1);
+
+            int result2 = Subtraction.Subtract(10, 4);
+            Console.WriteLine("Result of Subtraction: " + result2);
+        }
+    }
+}
+
+namespace MyMath
+{
+    public class Addition
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    public class Subtraction
+    {
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
