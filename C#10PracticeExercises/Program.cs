14b56d8ebadc9c9806564877bdaa1c97b910c7f5
Adum Awan <adummaker02@gmail.com>
2024-02-07 09:24:03 +0300

    Added Colors class with static fields and GetColor method

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Drawing;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Color color1 = Colors.GetColor("Red");
+            Console.WriteLine("Color 1: " + color1.Name);
+
+            Color color2 = Colors.GetColor("Green");
+            Console.WriteLine("Color 2: " + color2.Name);
+
+            Color color3 = Colors.GetColor("Blue");
+            Console.WriteLine("Color 3: " + color3.Name);
+
+            Color color4 = Colors.GetColor("Yellow");
+            Console.WriteLine("Color 4: " + color4.Name);
+        }
+    }
+}
+
+public class Colors
+{
+    public static Color Red = Color.Red;
+    public static Color Green = Color.Green;
+    public static Color Blue = Color.Blue;
+
+    public static Color GetColor(string colorName)
+    {
+        switch (colorName.ToLower())
+        {
+            case "red":
+                return Red;
+            case "green":
+                return Green;
+            case "blue":
+                return Blue;
+            default:
+                return Color.Black;
+        }
+    }
+}
