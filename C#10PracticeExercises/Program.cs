4500fc09e115a01d20787e468861d01cb5bc9863
Adum Awan <adummaker02@gmail.com>
2024-02-07 09:49:07 +0300

    Added Person class library and console application to display full name

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using PersonLibrary;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.FirstName = "Adum";
+        person.LastName = "Maker";
+        person.Age = 30;
+        person.EmailAddress = "adummaker02@gmail.com";
+
+        string fullName = person.GetFullName();
+        Console.WriteLine("Full Name: " + fullName);
+    }
+}
+
+
+namespace PersonLibrary
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string EmailAddress { get; set; }
+
+        public string GetFullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
