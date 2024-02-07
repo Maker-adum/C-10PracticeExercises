5f2df0d2e94d277193def01974375f11bcce71ee
Adum Awan <adummaker02@gmail.com>
2024-02-07 09:45:01 +0300

    Added program to prompt user for name with validation

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = string.Empty;
+
+        while (string.IsNullOrEmpty(name))
+        {
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Name cannot be empty. Please try again.");
+            }
+        }
+
+        Console.WriteLine("Hello, " + name + "! Welcome to the program.");
+    }
+}
