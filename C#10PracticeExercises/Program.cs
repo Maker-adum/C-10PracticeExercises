062822018813b6a56891f9e5c9a17aff236afbda
Adum Awan <adummaker02@gmail.com>
2024-02-07 09:41:57 +0300

    Added method to calculate string length with null handling

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = "Hello, world!";
+        int length = GetStringLength(input);
+        Console.WriteLine(length);
+    }
+
+    public static int GetStringLength(string input)
+    {
+        if (input == null)
+        {
+            return 0;
+        }
+        else
+        {
+            return input.Length;
+        }
+    }
+}
