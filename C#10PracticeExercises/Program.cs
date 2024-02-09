ffbc0b767157c7a6894326cdb844466b7a37da38
Adum Awan <adummaker02@gmail.com>
2024-02-09 14:12:28 +0300

    Calculate the average of elements in a double array using a for loop

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Creating an array of doubles
+        double[] myArray = { 1.5, 2.3, 4.7, 3.2, 5.1 };
+
+        // Calculating the average of the elements in the array
+        double sum = 0;
+        for (int i = 0; i < myArray.Length; i++)
+        {
+            sum += myArray[i];
+        }
+
+        double average = sum / myArray.Length;
+
+        // Printing the average
+        Console.WriteLine("Average: " + average);
+    }
+}
