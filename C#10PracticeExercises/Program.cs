2bc106df61ac20a7f5cafa17933cbb15534324e5
Adum Awan <adummaker02@gmail.com>
2024-02-09 14:17:43 +0300

    Print all elements of a two-dimensional integer array using nested loops

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // two-dimensional array of integers
+        int[,] myArray = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        // Printing all elements in the array
+        for (int i = 0; i < myArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < myArray.GetLength(1); j++)
+            {
+                Console.WriteLine(myArray[i, j]);
+            }
+        }
+    }
+}
