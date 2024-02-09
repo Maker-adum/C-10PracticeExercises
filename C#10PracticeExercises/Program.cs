d5e1c9f02d2b05db2a41dae6c72bfa2340d42947
Adum Awan <adummaker02@gmail.com>
2024-02-09 14:09:20 +0300

    Add code to create and print elements of an integer array

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] myArray = { 1, 2, 3, 4, 5 };
+
+        foreach (int element in myArray)
+        {
+            Console.WriteLine(element);
+        }
+
+        Console.WriteLine(myArray.Length);
+    }
+}
