2b86aaaa009ef192efa6039283812afe7acee9f0
Adum Awan <adummaker02@gmail.com>
2024-02-09 14:22:57 +0300

    Added favorite movies to an array, modify and print array elements

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Creating an array of strings
+        string[] movies = { "Bell Air", "The midnight Agent", "The Dark Desire", "Fight Club" };
+
+        // Adding another favorite movie using Array.Add
+        Array.Resize(ref movies, movies.Length + 1);
+        movies[movies.Length - 1] = "13 Reasons Why";
+
+        // Printing the second element using the indexer
+        Console.WriteLine("Second element: " + movies[1]);
+
+        // Sorting the array in alphabetical order
+        Array.Sort(movies);
+
+        // Printing the sorted array
+        Console.WriteLine("Sorted movies:");
+        foreach (string movie in movies)
+        {
+            Console.WriteLine(movie);
+        }
+    }
+}
