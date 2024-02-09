0f372b914d43d4e655a2c3f3bf591cc062f35a09
Adum Awan <adummaker02@gmail.com>
2024-02-09 14:30:43 +0300

    Implemented method to filter and return even numbers greater than or equal to 10

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static List<int> GetEvenNumbersGreaterThanTen(List<int> numbers)
+    {
+        List<int> result = new List<int>();
+
+        foreach (int number in numbers)
+        {
+            if (number >= 10 && number % 2 == 0)
+            {
+                result.Add(number);
+            }
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        // Example usage
+        List<int> numbers = new List<int> { 5, 10, 15, 20, 25, 30 };
+
+        List<int> evenNumbersGreaterThanTen = GetEvenNumbersGreaterThanTen(numbers);
+
+        // Printing the result
+        Console.WriteLine("Even numbers greater than or equal to 10:");
+        foreach (int number in evenNumbersGreaterThanTen)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
