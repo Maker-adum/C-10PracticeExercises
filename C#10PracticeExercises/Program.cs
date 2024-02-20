9d367be8e6e20784ae124f120357ac8b8f9c8e50
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-02-20 16:05:48 +0300

    "Add Animal base class and Mammals derived class with properties and methods"

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Animal
+{
+    public string Name { get; set; }
+    public double Weight { get; set; }
+
+    public void Eat()
+    {
+        Console.WriteLine($"{Name} is eating.");
+    }
+
+    public void Sleep()
+    {
+        Console.WriteLine($"{Name} is sleeping.");
+    }
+}
+
+public class Mammals : Animal
+{
+    public int NumberOfLegs { get; set; }
+
+    public void Move()
+    {
+        Console.WriteLine($"{Name} is moving on {NumberOfLegs} legs.");
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Mammals dog = new Mammals();
+        dog.Name = "Dog";
+        dog.Weight = 15.5;
+        dog.NumberOfLegs = 4;
+
+        dog.Eat();
+        dog.Sleep();
+        dog.Move();
+    }
+}
