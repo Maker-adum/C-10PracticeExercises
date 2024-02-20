4a3bf9002ba5be50a1b921a88414db0a9d45b3da
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-02-20 16:14:15 +0300

    "Add Shape base class with overridden Draw method in Rectangle and Circle classes"

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+public class Shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing a generic shape.");
+    }
+}
+
+public class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle.");
+    }
+}
+
+public class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle.");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Rectangle());
+        shapes.Add(new Circle());
+
+        foreach (Shape shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
