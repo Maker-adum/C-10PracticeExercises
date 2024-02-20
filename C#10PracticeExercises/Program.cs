62db519b29462a9e008808c1b546ac96f64498e0
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-02-20 16:19:17 +0300

    "Add IDrawable interface and implement Draw method in Circle, Rectangle, and Triangle classes"

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public interface IDrawable
+{
+    void Draw();
+}
+
+public class Circle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a circle.");
+    }
+}
+
+public class Rectangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle.");
+    }
+}
+
+public class Triangle : IDrawable
+{
+    public void Draw()
+    {
+        Console.WriteLine("Drawing a triangle.");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<IDrawable> shapes = new List<IDrawable>();
+
+        shapes.Add(new Circle());
+        shapes.Add(new Rectangle());
+        shapes.Add(new Triangle());
+
+        foreach (IDrawable shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
