ebf6aa00983f922826bb615a0852d7c435abfbae
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-02-20 16:12:16 +0300

    "Add Car class with Engine composition and ElectricCar class as a derived class"

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Engine
+{
+    public void Start()
+    {
+        Console.WriteLine("Engine started.");
+    }
+}
+
+public class Car
+{
+    private Engine engine;
+
+    public Car()
+    {
+        engine = new Engine();
+    }
+
+    public void StartEngine()
+    {
+        engine.Start();
+    }
+}
+
+public class ElectricCar : Car
+{
+    public int BatteryCapacity { get; set; }
+    public string Manufacturer { get; set; }
+    public string Model { get; set; }
+    public int RangePerCharge { get; set; }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.StartEngine();
+
+        ElectricCar electricCar = new ElectricCar();
+        electricCar.StartEngine();
+    }
+}
