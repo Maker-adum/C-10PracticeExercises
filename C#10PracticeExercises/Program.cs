724d2fa53d2d7ec5a65b6784b2a50163c7307b9f
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-02-20 16:03:11 +0300

    "Add Person class with private fields and public getter/setter methods"

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Person
+{
+    private string name;
+    private int age;
+    private string address;
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public void SetName(string newName)
+    {
+        name = newName;
+    }
+
+    public int GetAge()
+    {
+        return age;
+    }
+
+    public void SetAge(int newAge)
+    {
+        age = newAge;
+    }
+
+    public string GetAddress()
+    {
+        return address;
+    }
+
+    public void SetAddress(string newAddress)
+    {
+        address = newAddress;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person person = new Person();
+        person.SetName("Abraham Luke");
+        person.SetAge(30);
+        person.SetAddress("123 Lodwar Town");
+
+        Console.WriteLine($"Name: {person.GetName()}");
+        Console.WriteLine($"Age: {person.GetAge()}");
+        Console.WriteLine($"Address: {person.GetAddress()}");
+    }
+}
