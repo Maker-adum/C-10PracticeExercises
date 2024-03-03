333d34665eaf478a3e4f4dbe7d76ab024abccad4
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-03 15:08:35 +0300

    program to prompt for user name and age. Print customized greeting with input.

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Please enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        string message = $"Hello {name}, you are {age} years old!";
+        Console.WriteLine(message);
+
+        Console.ReadKey();
+    }
+}
+
