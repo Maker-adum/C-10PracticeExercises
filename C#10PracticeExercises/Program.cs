2dd04e36db38997ada9e06d66082921657724e54
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-12 20:35:58 +0300

    Created program with debugger to observe user input parsing and variable values

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        string userInput = Console.ReadLine();
+
+        // Set a breakpoint here to observe the value of userInput as it is parsed
+
+        int parsedInput = int.Parse(userInput);
+
+        int result = parsedInput * 2;
+
+        Console.WriteLine($"The result is: {result}");
+    }
+}
