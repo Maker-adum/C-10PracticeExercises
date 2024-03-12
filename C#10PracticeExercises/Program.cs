17fabf05b106de5eae040c9ada61909c1e5c7e40
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-12 20:29:06 +0300

    calculate factorial and demonstrate debugger usage

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int factorial = CalculateFactorial(number);
+
+        Console.WriteLine($"The factorial of {number} is {factorial}.");
+    }
+
+    static int CalculateFactorial(int number)
+    {
+        // Set a breakpoint here to check the initial value of the number
+
+        if (number == 0 || number == 1)
+        {
+            // Set a breakpoint here to check the factorial calculation for 0 and 1
+            return 1;
+        }
+
+        int result = number;
+
+        while (number > 1)
+        {
+            // Set a breakpoint here to step through the factorial calculation loop
+            number--;
+            result *= number;
+        }
+
+        // Set a breakpoint here to check the final result before returning
+        return result;
+    }
+}
