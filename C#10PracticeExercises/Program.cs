04e616a0d8676abcce670340a0de2050d90ffcb4
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-26 17:41:41 +0300

    exercise 2

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "input.txt";
+
+        // Read the contents of the file
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string fileContents = reader.ReadToEnd();
+
+            // Print the contents to the console
+            Console.WriteLine("File contents:");
+            Console.WriteLine(fileContents);
+        }
+
+        // The program continues executing after the using block
+        Console.WriteLine("File reading complete.");
+    }
+}
