aec2d26e64e400879373691b6d0661cbd8a3eb42
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-26 17:32:55 +0300

    "Fix file path and file access issues in program"

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "output.txt";
+
+        // Create the file and write the text
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine("Hello, World!");
+        }
+
+        // Verify the text was written by reading the file contents
+        string fileContents = File.ReadAllText(filePath);
+        Console.WriteLine(fileContents);
+    }
+}
