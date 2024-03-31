256e3eaec89b7e2939f16ae424a8d5309ca221e9
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-31 11:47:17 +0300

    used binary writer to write into a file and binary reader to read from a file

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "output.bin";
+
+        using (BinaryWriter writer = new BinaryWriter(File.Open(filePath, FileMode.Create)))
+        {
+            writer.Write(1);
+            writer.Write(2);
+            writer.Write(3);
+            writer.Write(4);
+        }
+
+        using (BinaryReader reader = new BinaryReader(File.Open(filePath, FileMode.Open)))
+        {
+            Console.WriteLine("Integers read from the binary file:");
+            Console.WriteLine(reader.ReadInt32());
+            Console.WriteLine(reader.ReadInt32());
+            Console.WriteLine(reader.ReadInt32());
+            Console.WriteLine(reader.ReadInt32());
+        }
+    }
+}
