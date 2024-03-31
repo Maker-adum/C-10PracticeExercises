5e9519f3e887801081622d394c855de82a163e6f
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-31 14:15:55 +0300

    Add directory creation and file listing functionality

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string mainDirectory = "mydir";
+        string subDirectory = "mysubdir";
+
+        if (!Directory.Exists(mainDirectory))
+        {
+            Directory.CreateDirectory(mainDirectory);
+            Console.WriteLine($"Created directory: {mainDirectory}");
+        }
+
+        string subDirectoryPath = Path.Combine(mainDirectory, subDirectory);
+        Directory.CreateDirectory(subDirectoryPath);
+        Console.WriteLine($"{subDirectory}");
+
+        string[] filesInmainDirectory = Directory.GetFiles(mainDirectory);
+
+        foreach (string file in filesInmainDirectory)
+        {
+            Console.WriteLine(Path.GetFileName(file));
+        }
+    }
+}
