d6844f7d717bec0df8cd5755cced483434f8a9a8
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-31 11:12:34 +0300

    Copied, moved and checked if files exist

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string sourceFilePath = "source.txt";
+        string destinationFilePath = "destination.txt";
+        string newnameFilePath = "newname.txt";
+
+
+        using (StreamWriter writer = new StreamWriter(sourceFilePath))
+        {
+            writer.WriteLine("Hello again world!");
+        }
+
+        File.Copy(sourceFilePath, destinationFilePath);
+
+        File.Move(destinationFilePath, newnameFilePath);
+
+        Console.WriteLine("Existence of files:");
+        Console.WriteLine($"source.txt exists: {File.Exists(sourceFilePath)}");
+        Console.WriteLine($"destinarion.txt exists: {File.Exists(destinationFilePath)}");
+        Console.WriteLine($"newname.txt exists: {File.Exists(newnameFilePath)}");
+    }
+}
