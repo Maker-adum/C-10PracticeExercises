6133d2a1cd7d43f926560763779e6a35456ac47d
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-31 14:47:58 +0300

    Word count functionality

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a sentence: ");
+        string sentence = Console.ReadLine();
+
+        sentence = sentence.Trim();
+
+        string[] words = Regex.Split(sentence, @"\s+");
+
+        int wordCount = words.Length;
+
+        Console.WriteLine($"Number of words in the sentence: {wordCount}");
+    }
+}
