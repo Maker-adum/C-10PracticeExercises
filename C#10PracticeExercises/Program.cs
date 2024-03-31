832fb2bdae66897eb7048953b16120d21c1fd302
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-03-31 14:29:46 +0300

    Updated the file reading logic in the console application to remove unnecessary WriteLine() call before reading the file path.

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the file path: ");
+        string filePath = Console.ReadLine();
+
+        if (File.Exists(filePath))
+        {
+            using (FileStream fileStream = File.OpenRead(filePath))
+            {
+                byte[] buffer = new byte[fileStream.Length];
+
+                int bytesRead = fileStream.Read(buffer, 0, buffer.Length);
+
+                string fileContents = System.Text.Encoding.UTF8.GetString(buffer);
+                Console.WriteLine("File Contents");
+                Console.WriteLine(fileContents);
+            }
+        }
+
+        else
+        {
+            Console.WriteLine("File not found");
+        }
+    }
+}
