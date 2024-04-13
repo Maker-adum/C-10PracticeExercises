7667b2a03c8dca510f741b90c3d326befcdd05c4
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-04-13 13:29:22 +0300

    console application to read and display file contents, with error handling for FileNotFound exception

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter the file path: ");
+            string filePath = Console.ReadLine();
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException();
+            }
+
+            string fileContents = File.ReadAllText(filePath);
+            Console.WriteLine("File contents");
+            Console.WriteLine(fileContents);
+        }
+
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Error: File not found. ");
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
