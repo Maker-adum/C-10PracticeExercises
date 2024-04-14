1ca5e66540b73ca3d5671217e9e9d94c592948f1
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-04-14 22:16:41 +0300

    Implemented a guessing game where the user guesses a randomly generated number between 1 and 100, with error handling for user input.

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(1, 101);
+
+        bool guessedCorrectly = false;
+        while (!guessedCorrectly)
+        {
+            Console.WriteLine("Guess the number between 1 and 100:");
+            try
+            {
+                int userGuess = Convert.ToInt32(Console.ReadLine());
+
+                if (userGuess == randomNumber)
+                {
+                    Console.WriteLine("Congratulations! You guessed the number correctly.");
+                    guessedCorrectly = true;
+                }
+                else if (userGuess < randomNumber)
+                {
+                    Console.WriteLine("Try again. The number is higher.");
+                }
+                else
+                {
+                    Console.WriteLine("Try again. The number is lower.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Input is not a valid integer.");
+            }
+        }
+    }
+}
