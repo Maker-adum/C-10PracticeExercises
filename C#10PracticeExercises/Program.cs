c84798e62a5b9d6e32931bf6f646e173d72ef995
Adum-maker <99293825+Adum-maker@users.noreply.github.com>
2024-04-14 21:57:40 +0300

    program to accept user input and convert it to a double, handling FormatException for non-numeric input.

--- C#10PracticeExercises/Program.cs
+++ C#10PracticeExercises/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number");
+
+        try
+        {
+            string userInput = Console.ReadLine();
+
+            double number = Convert.ToDouble(userInput);
+
+            Console.WriteLine("The number you entered is : " + number);
+        }
+
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Input is not a valid number.");
+        }
+    }
+}
